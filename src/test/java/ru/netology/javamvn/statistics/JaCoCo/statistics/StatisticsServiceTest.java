0159db5cbe7e13f80ec8c100fc42fa7592454912
Valeria Mironova <easymiroo@gmail.com>
2025-12-04 10:20:44 +0400

    Add additional tests for 100%

--- src/test/java/ru/netology/javamvn/statistics/JaCoCo/statistics/StatisticsServiceTest.java
+++ src/test/java/ru/netology/javamvn/statistics/JaCoCo/statistics/StatisticsServiceTest.java
@@ -17,4 +17,40 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMax_singleElement() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomes = {5};
+        long expected = 5;
+
+        long actual = service.findMax(incomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMax_allSame() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomes = {3, 3, 3};
+        long expected = 3;
+
+        long actual = service.findMax(incomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    void findMax_negativeNumbers() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomes = {-5, -1, -10};
+        long expected = -1;
+
+        long actual = service.findMax(incomes);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
