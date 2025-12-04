83dbd7aed6e250d2dcf8b2597ea8425f4901e388
Valeria Mironova <easymiroo@gmail.com>
2025-12-04 10:08:19 +0400

    Initial commit

---
+++ src/main/java/ru/netology/javamvn/statistics/JaCoCo/statistics/StatisticsService.java
@@ -0,0 +1,13 @@
+package ru.netology.javamvn.statistics.JaCoCo.statistics;
+
+public class StatisticsService {
+    public long findMax(long[] incomes) {
+        long currentMax = incomes[0];
+        for (long income : incomes) {
+            if (currentMax < income) {
+                currentMax = income;
+            }
+        }
+        return currentMax;
+    }
+}
